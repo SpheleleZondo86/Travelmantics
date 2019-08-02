c4cfdcd2cce8ce2ca6c26d69ea6d1ede0f552b4d
Sphelele Zondo <zondos@elections.org.za>
2019-08-02 13:09:55 +0200

    initial commit

---
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -0,0 +1,77 @@
+package com.example.travelmantics;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InsertActivity extends AppCompatActivity {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    EditText txtTitle;
+    EditText txtPrice;
+    EditText txtDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("traveldeals");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtDescription = findViewById(R.id.txtDescription);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_menu:
+                saveDeal();
+                clean();
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String price = txtPrice.getText().toString();
+        String description = txtDescription.getText().toString();
+        TravelDeal travelDeal = new TravelDeal(title, price, description, "");
+        databaseReference.push().setValue(travelDeal).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Deal saved!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+}
